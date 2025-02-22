7bf3b403a2c37d628536748ddc7336d3baead0a9
ConeZ123 <90795379+ConeZ123@users.noreply.github.com>
2025-02-22 12:17:03 +0100

    Update zad1.c

--- lab1/zad1.c
+++ lab1/zad1.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     printf("Podaj wartość n: ");
     scanf("%d", &n);
 
-    int hex_mode = (argc > 1) ? 1 : 0;
+    int hex_mode = (argc > 1);
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
