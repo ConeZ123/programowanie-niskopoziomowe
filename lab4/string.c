5b099d969c720399762a05891ba10dc572004505
ConeZ123 <nismat2004@gmail.com>
2025-03-13 14:08:39 +0100

    Create string.c

---
+++ lab4/string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int pn_str_len(const char *str) {}
+
+bool pn_str_cmp(const char *a, const char * b) {}
+
+void pn_str_capitalize(char *str) {}
+
+void pn_str_trim(char *str) {}
+
+void pn_str_rev(char *str) {}
